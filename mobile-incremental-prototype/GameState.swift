fbcbfa087d58edbeab086b6cc99b13d01414e07c
davidrynn <davidrynn@gmail.com>
2026-01-18 12:47:11 -0500

    Add upgrade system to core engine

--- mobile-incremental-prototype/GameState.swift
+++ mobile-incremental-prototype/GameState.swift
@@ -11,11 +11,17 @@ enum ActionType: Equatable {
     case primaryTap
 }
 
+enum UpgradeType: Equatable {
+    case primaryYield
+}
+
 struct GameState: Equatable {
     var resource: Int
+    var primaryYieldLevel: Int
 
-    init(resource: Int = 0) {
+    init(resource: Int = 0, primaryYieldLevel: Int = 0) {
         self.resource = resource
+        self.primaryYieldLevel = primaryYieldLevel
     }
 }
 
@@ -24,7 +30,31 @@ func apply(action: ActionType, to state: GameState) -> GameState {
 
     switch action {
     case .primaryTap:
-        nextState.resource += 1
+        nextState.resource += 1 + nextState.primaryYieldLevel
+    }
+
+    return nextState
+}
+
+func upgradeCost(for upgrade: UpgradeType, atLevel level: Int) -> Int {
+    switch upgrade {
+    case .primaryYield:
+        return 10 * (level + 1)
+    }
+}
+
+func purchase(upgrade: UpgradeType, in state: GameState) -> GameState {
+    var nextState = state
+
+    switch upgrade {
+    case .primaryYield:
+        let cost = upgradeCost(for: upgrade, atLevel: nextState.primaryYieldLevel)
+        guard nextState.resource >= cost else {
+            return nextState
+        }
+
+        nextState.resource -= cost
+        nextState.primaryYieldLevel += 1
     }
 
     return nextState
